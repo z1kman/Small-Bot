1ce4a94c4bf5d4f33c387fec8a82b6fd77c2914d
z1kman <z1kmanfast@gmail.com>
2020-03-15 21:24:49 +0300

    Начал делать создание условий

--- project/Constructor/js/Visual-Part/Dev.js
+++ project/Constructor/js/Visual-Part/Dev.js
@@ -1,4 +1,15 @@
+//Используется  NumberOfElement(id) из Service.js
+//Используется  SecondNumberOfElement(id) из Service.js
+//Используется  CreateWindowPanel() из Service.js
+//Используется  DisabledNavbarBtn() из Service.js
 
-//---------------Тесты----------------
-//------------------------------------
-//------------------------------------
+
+function OnClickAddConditionBtn(id){
+    CreateWindowPanel();
+    DisabledNavbarBtn();
+    
+    let N = NumberOfElement(id);
+    let SN = SecondNumberOfElement(id);
+    let AddNewPanel = document.getElementById("AddNewPanel");
+    
+}
