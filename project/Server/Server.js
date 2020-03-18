d82015a90dc38d31a54feafcc08a75eb052ae5b3
z1kman <z1kmanfast@gmail.com>
2020-03-18 19:57:01 +0300

    Исправил ошибки в маршрутизации

--- project/Server/Server.js
+++ project/Server/Server.js
@@ -1,10 +1,5 @@
 const express = require("../Expressapp/node_modules/express");
 const app = express();
-app.get("/", function(request, response){
-    let Path = __dirname.substr(0, __dirname.length - 6);
-    response.sendFile(Path + "index.html");
-});
-app.use("/", express.static("../"));
 
 app.get("/main", function(request, response){
     let Path = __dirname.substr(0, __dirname.length - 6);
@@ -16,7 +11,7 @@ app.get("/constructor", function(request, response){
     let Path = __dirname.substr(0, __dirname.length - 6);
     response.sendFile(Path + "Constructor/constructor.html");
 });
-app.use("/constructor", express.static("../"));
+app.use("/constructor", express.static("../Constructor"));
 
 app.get("/contacts", function(request, response){
     let Path = __dirname.substr(0, __dirname.length - 6);
@@ -29,4 +24,6 @@ app.get("/login", function(request, response){
     response.sendFile(Path + "LoginForm/LoginForm.html");
 });
 app.use("/login", express.static("../"));
+
+
 app.listen(3000);
