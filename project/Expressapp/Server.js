6d7a4e4fb5372c7e6e077d9255bf39ffc5dfeaf0
z1kman <z1kmanfast@gmail.com>
2020-03-19 16:08:24 +0300

    Добавлен парсер, сделано считывание информации с полей ввода(страница входа)

--- project/Expressapp/Server.js
+++ project/Expressapp/Server.js
@@ -1,8 +1,11 @@
 const express = require("../Expressapp/node_modules/express");
 const app = express();
-let path = require('path');
+var path = require('path');
+var bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({extended: false});//создание парсера
 
 app.use(express.static(path.join(__dirname, 'public')));//подключение css/js и source файлов
+
 app.get("/main", function(request, response){
     response.sendFile(__dirname + "/views/index.html");
 });
@@ -13,8 +16,13 @@ app.get("/contacts", function(request, response){
     response.sendFile(__dirname +  "/views/contacts.html");
 });
 app.get("/login", function(request, response){
-    //let Path = __dirname.substr(0, __dirname.length - 6);
     response.sendFile(__dirname + "/views/LoginForm.html");
 });
-
+app.post(("/login"), urlencodedParser, function(request, response){
+    if(!request.body){
+        return response.sendStatus(400);
+    }
+    console.log(request.body);//данные введеные на странице логина
+    response.sendFile(__dirname + "/views/LoginForm.html");
+});
 app.listen(3000);
